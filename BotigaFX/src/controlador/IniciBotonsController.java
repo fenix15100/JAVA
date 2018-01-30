dce8ae59348bd4ce15fea54ed61390910deaffcc
fenix15100 <francisco.camacho15@gmail.com>
2018-01-30 17:04:40 +0100

    La madre del juan es puta

--- BotigaFX/src/controlador/IniciBotonsController.java
+++ BotigaFX/src/controlador/IniciBotonsController.java
@@ -5,6 +5,8 @@ import java.util.ResourceBundle;
 import java.util.Locale.Category;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -48,5 +50,19 @@ public class IniciBotonsController extends Application {
 		
 
 	}
+	
+	
+	 @FXML
+	 public void btnProductos(ActionEvent event) {
+		 
+		 
 
+	 }
+
+	 @FXML
+	 public void btnSalir(ActionEvent event) {
+		 
+		 Platform.exit();
+
+	 }
 }
