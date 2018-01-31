177cc8447a27ef56bc3a3b0014bcafef7c75abac
fenix15100 <francisco.camacho15@gmail.com>
2018-01-31 12:56:50 +0100

    ProductView 90%
    controllerView 10%

--- BotigaFX/src/controlador/IniciBotonsController.java
+++ BotigaFX/src/controlador/IniciBotonsController.java
@@ -1,5 +1,7 @@
 package controlador;
 
+import java.io.IOException;
+
 import java.util.Locale;
 import java.util.ResourceBundle;
 import java.util.Locale.Category;
@@ -7,15 +9,20 @@ import java.util.Locale.Category;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 
 public class IniciBotonsController extends Application {
+	//Cargo la traduccion en el controlador
+	private Locale localitzacioDisplay = Locale.getDefault(Category.DISPLAY);
+	private ResourceBundle texts = ResourceBundle.getBundle("vista.Texts", localitzacioDisplay);
 	
 	@FXML
     private AnchorPane root;
@@ -32,10 +39,6 @@ public class IniciBotonsController extends Application {
 		//Cargo la vista asociada al controlador Principal
 		FXMLLoader loaderview = new FXMLLoader(getClass().getResource("/vista/IniciBotonsView.fxml"));
 		
-		//Cargo la traduccion en el controlador
-		Locale localitzacioDisplay = Locale.getDefault(Category.DISPLAY);
-		ResourceBundle texts = ResourceBundle.getBundle("vista.Texts", localitzacioDisplay);
-		
 		//Cargo la traducion a la vista
 		loaderview.setResources(texts);
 		
@@ -44,7 +47,7 @@ public class IniciBotonsController extends Application {
 		
 		//Asigno la escena al Escenario principal y abro el telon
 		escenarioPrincipal.setScene(escenaPrincipal);
-		escenarioPrincipal.setTitle(texts.getString("title.botiga"));
+		escenarioPrincipal.setTitle(texts.getString("Inicio.title.botiga"));
 		escenarioPrincipal.show();
 		
 		
@@ -53,8 +56,36 @@ public class IniciBotonsController extends Application {
 	
 	
 	 @FXML
-	 public void btnProductos(ActionEvent event) {
+	 public void OnActionbtnProductos(ActionEvent event) throws IOException {
 		 
+		
+		 //Cargo en memoria la vista ProductosView y la cargo dentro de un Panel 
+			FXMLLoader loaderview = new FXMLLoader(getClass().getResource("/vista/ProductosView.fxml"));
+			AnchorPane Panel = (AnchorPane)loaderview.load();
+	
+			
+			//Creo una nueva Ventana
+			Stage escenarioProductos = new Stage();
+			//Creo una escena y meto dentro el panel con la vista
+			Scene escenaProductos = new Scene(Panel);
+			
+			escenarioProductos.setTitle("Productos");
+			
+			//Cargo la escena en el escenario y muestro el escenario
+			escenarioProductos.setScene(escenaProductos);
+			escenarioProductos.show();
+			
+			//Le entrego el escenario a su controlador
+		
+			ProductosController productoControler = (ProductosController)loaderview.getController();
+			productoControler.setVentana(escenarioProductos);
+		 
+			//Handle para el evento de cierre del escenario
+			escenarioProductos.setOnCloseRequest(new EventHandler<WindowEvent>() {
+				public void handle(WindowEvent we) {
+					productoControler.salir();
+				}
+			});   
 		 
 		 
 		 
@@ -62,7 +93,7 @@ public class IniciBotonsController extends Application {
 	 }
 
 	 @FXML
-	 public void btnSalir(ActionEvent event) {
+	 public void OnActionbtnSalir(ActionEvent event) {
 		 
 		 Platform.exit();
 
---
+++ BotigaFX/src/controlador/ProductosController.java
@@ -0,0 +1,67 @@
+package controlador;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import modelo.*;
+
+public class ProductosController {
+	
+	private Stage ventana;
+	
+	//Inyecion de los elementos de la Vista
+	@FXML
+    private AnchorPane root;
+
+    @FXML
+    private TextField idTextfield;
+
+    @FXML
+    private TextField nomTexfield;
+
+    @FXML
+    private TextField stockTexfield;
+
+    @FXML
+    private DatePicker I_catalogoDatePicker;
+
+    @FXML
+    private DatePicker f_catalogoDatePicker;
+
+    @FXML
+    private ComboBox<String> tipoComboBox;
+	
+	
+    
+    
+	
+	@FXML private void initialize() {
+		
+		//Inyecto en el ComboBox dos Values.
+		tipoComboBox.getItems().addAll("Joc","Pack");
+		
+		
+		
+	
+		
+	}
+	
+	public Stage getVentana() {
+		return ventana;
+	}
+
+	public void setVentana(Stage ventana) {
+		this.ventana = ventana;
+	}
+
+	public void salir() {
+		//HERE SAVE Productos and exit
+		ventana.close();
+		
+	}
+
+}
