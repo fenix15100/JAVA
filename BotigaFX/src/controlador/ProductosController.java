1b4378a8fd782af7967958190edd08b098b47ecd
fenix15100 <francisco.camacho15@gmail.com>
2018-01-31 16:10:32 +0100

    Skeleton Controller Products DONE

--- BotigaFX/src/controlador/ProductosController.java
+++ BotigaFX/src/controlador/ProductosController.java
@@ -3,10 +3,13 @@ package controlador;
 
 import java.io.IOException;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import modelo.*;
@@ -48,12 +51,50 @@ public class ProductosController {
 		tipoComboBox.getItems().addAll("Joc","Pack");
 		
 		//Instancio el DAO Productos y cargo la persitencia de datos
-		
 		dao_productos.loadData();
 		
 		
 	}
 	
+	
+	@FXML
+	public void OnActionbtnGuardar(ActionEvent event) throws IOException {
+	
+	}
+	
+	@FXML
+	public void OnActionbtnEliminar(ActionEvent event) throws IOException {
+		
+	}
+	
+	@FXML 
+	public void OnActionbtnSalir(ActionEvent event) throws IOException {
+		
+	}
+	
+	@FXML 
+	private void onKeyPressedId(KeyEvent e) throws IOException {
+		if (e.getCode() == KeyCode.ENTER || e.getCode() == KeyCode.TAB){
+			
+		}else {
+			//Nuevo registro
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//Metodos para cargar una ventana al controlador
 	public Stage getVentana() {
 		return ventana;
 	}
@@ -62,6 +103,8 @@ public class ProductosController {
 		this.ventana = ventana;
 	}
 
+	
+	//Metodo que se ejecuta al salir de la aplicacion
 	public void salir() throws IOException {
 		
 		//Guarda la estructura de datos en un fichero y cierra el escenario
