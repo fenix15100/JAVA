f8ddf1bd676ec35044bf28c1938985aa6a691df0
fenix15100 <francisco.camacho15@gmail.com>
2018-03-22 19:51:06 +0100

    trtrt

---
+++ Test/src/Mayor3.java
@@ -0,0 +1,14 @@
+
+public class Mayor3 {
+	
+	
+    public int numero_mayor(int a, int b, int c) {
+        if (a > b && a > c) {
+            return a;
+        } else if (c > b) {
+            return c;
+        } else {
+            return b;
+        }
+    }
+}
